bbd54acb8c90d0d323818f2fb87d7d53ed80f130
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-06-26 15:39:47 +0200

    Replace debug asserts for async unimplemented with proper unimplemented!-s

--- crates/kotlin/src/lib.rs
+++ crates/kotlin/src/lib.rs
@@ -278,7 +278,9 @@ impl WorldGenerator for Kotlin {
         _files: &mut Files,
     ) {
         for (name, func) in funcs {
-            debug_assert!(func.kind != FunctionKind::AsyncFreestanding);
+            if func.kind == FunctionKind::AsyncFreestanding {
+                unimplemented!("async unimplemented");
+            }
 
             // only add in place funcs if they're freestanding, otherwise they are part of a resource
 
@@ -300,7 +302,9 @@ impl WorldGenerator for Kotlin {
         _files: &mut Files,
     ) -> Result<()> {
         for (name, func) in funcs {
-            debug_assert!(func.kind != FunctionKind::AsyncFreestanding);
+            if func.kind == FunctionKind::AsyncFreestanding {
+                unimplemented!("async unimplemented");
+            }
 
             // see import_funcs for explanation
 
@@ -2549,7 +2553,9 @@ impl Bindgen for FunctionBindgen<'_, '_> {
 
             Instruction::CallInterface { func, async_ } => {
                 // TODO async
-                debug_assert_eq!(*async_, false);
+                if *async_ {
+                    unimplemented!("async unimplemented");
+                }
                 let (assignment, destructure) = match func.result {
                     None => (String::new(), String::new()),
                     Some(ty) => {
